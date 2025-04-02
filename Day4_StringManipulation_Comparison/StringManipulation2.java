1e1fc98d3737a2e71dcedd5a2e889a0b8949be1a
vinhphuphan <vinhphu.phan@students.mq.edu.au>
2025-04-02 22:44:03 +1000

    Update day 4 content

---
+++ Day4_StringManipulation_Comparison/StringManipulation2.java
@@ -0,0 +1,14 @@
+public class StringManipulation2 {
+    public static void main(String[] args) {
+        // substring()
+        String phrase = "Programming in Java";
+        String word1 = phrase.substring(5);
+        String word2 = phrase.substring(0,11);
+        
+        System.out.println("Word 1 is : " + word1);
+        System.out.println("Word 2 is : " + word2);
+
+        
+    }   
+
+}

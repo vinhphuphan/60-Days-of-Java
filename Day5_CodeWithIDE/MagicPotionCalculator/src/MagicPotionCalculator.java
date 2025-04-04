955ea31aabbeb0cdf0403cf29a712d2805ea4afc
vinhphuphan <vinhphu.phan@students.mq.edu.au>
2025-04-04 20:26:20 +1000

    Learning java day 5

---
+++ Day5_CodeWithIDE/MagicPotionCalculator/src/MagicPotionCalculator.java
@@ -0,0 +1,10 @@
+public class MagicPotionCalculator {
+    public static void main(String[] agrs) {
+        int magicEssence = 5;
+        int dragonScale = 3;
+        int unicornHair = 2;
+        int potionPower = magicEssence * dragonScale + unicornHair;
+
+
+    }
+}

50c4199aa70f395c56a3cff6d57e03b2683383c0
vinhphuphan <vinhphu.phan@students.mq.edu.au>
2025-04-04 20:56:15 +1000

    Learning Java Day 6 - Control Statement

---
+++ Day6_ControlStatement/ifStatement.java
@@ -0,0 +1,11 @@
+public class ifStatement {
+    public static void main(String[] args) {
+        boolean isSunny = true;
+
+        if (isSunny) {
+            System.out.println("Such a beautiful day!");
+        } else {
+            System.out.println("Today is a bad day!");
+        }
+    }
+}

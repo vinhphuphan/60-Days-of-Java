d6c232f859809571e990f4683be5e1f6473cc8a1
vinhphuphan <vinhphu.phan@students.mq.edu.au>
2025-04-06 11:18:03 +1000

    Learning Java Day 7 - Array

--- Day7_Array/IterateThroughArray.java
+++ Day7_Array/IterateThroughArray.java
@@ -6,5 +6,17 @@ public interface IterateThroughArray {
         for (int i = 0; i < numbers.length; i++) {
             System.out.printf("%d " , numbers[i]);
         }
+        
+        System.out.println();
+
+        // enhanced for loop
+        for (int number : numbers) {
+            System.out.printf("%d " , number);
+        }
+        System.out.println();
+        String[] friends = {"Trang", "Duc", "Pin","Dong", "Dinh","Thinh", "Huy"};
+        for (String friend : friends) {
+            System.out.println(friend + " is my friend!");
+        }
     }
 }

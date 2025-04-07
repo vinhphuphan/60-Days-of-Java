8764fcb67a8ce63cdac2b0541e98b4089597f2cd
vinhphuphan <vinhphu.phan@students.mq.edu.au>
2025-04-07 21:32:55 +1000

    Learning Java Day 8 - 2D Array

---
+++ Day8_2D_Array/ArrayExample.java
@@ -0,0 +1,26 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[][] my2DArray = new int[3][3];
+        my2DArray[0][0] = 1;
+        my2DArray[0][1] = 2;
+        my2DArray[0][2] = 3;
+        my2DArray[1][0] = 4;
+        my2DArray[1][1] = 5;
+        my2DArray[1][2] = 6;
+        my2DArray[2][0] = 7;
+        my2DArray[2][1] = 8;
+        my2DArray[2][2] = 9;
+
+        // for (int i = 0; i < my2DArray.length; i++) {
+        //     for (int j = 0; j < my2DArray[i].length; j++) {
+        //         System.out.println(my2DArray[i][j] + " ");
+        //     }
+        // }
+
+        for ( int[] row : my2DArray) {
+            for ( int element : row) {
+                System.out.println(element);
+            }
+        }
+    }
+}
